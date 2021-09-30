467778981460cc3be611bd687c35c9279006ff92
Husan <abdigafurovhusan@gmail.com>
2021-09-30 19:58:02 +0500

    added actionTypes actions reducer and redux-devtools

--- src/index.js
+++ src/index.js
@@ -1,56 +1,73 @@
-import { compose, pipe } from 'lodash/fp';
-import { Map } from 'immutable';
-import { produce } from 'immer';
-let input = ' Javascript '
+// import { compose, pipe } from 'lodash/fp';
+// import { Map } from 'immutable';
+// import { produce } from 'immer';
+// let input = ' Javascript '
 
-const trim = str => str.trim();
-const toLowerCase = str => str.toLowerCase();
-const wrap = type => str => `<${type}>${str}</${type}>`
+// const trim = str => str.trim();
+// const toLowerCase = str => str.toLowerCase();
+// const wrap = type => str => `<${type}>${str}</${type}>`
 // let res = wrap('div')(toLowerCase(trim(input)))
-let transform = pipe(trim, toLowerCase, wrap('div'))
+// let transform = pipe(trim, toLowerCase, wrap('div'))
 // console.log(transform(input));
 
 
-const obj1 = {
-    name: 'Husan',
-    address: {
-        city: 'Tashkent'
-    }
-}
+// const obj1 = {
+//     name: 'Husan',
+//     address: {
+//         city: 'Tashkent'
+//     }
+// }
 
-const obj2 = {
-    ...obj1,
-    address: {
-        ...obj1.address
-    }
-}
-obj2.address.city = 'Samarkand';
+// const obj2 = {
+//     ...obj1,
+//     address: {
+//         ...obj1.address
+//     }
+// }
+// obj2.address.city = 'Samarkand';
 
 
 //immutable js start//
-let user = Map({ name: 'Husan' });
+// let user = Map({ name: 'Husan' });
 
-function change(user) {
-    return user.set('isUsed', true)
-}
-user = change(user)
+// function change(user) {
+//     return user.set('isUsed', true)
+// }
+// user = change(user)
 // console.log(user.toJS());
 
 //immutable js end//
 
 //immer js start//
 
-const book = { name: 'Harry Potter' };
+// const book = { name: 'Harry Potter' };
 
-function publish() {
-    return produce(book, draftBook => {
-        draftBook.ispublished = true
-    })
-}
+// function publish() {
+//     return produce(book, draftBook => {
+//         draftBook.ispublished = true
+//     })
+// }
 
-const updated = publish();
+// const updated = publish();
 
 // console.log(book);
 // console.log(updated);
 
 //immer js end//
+
+import store from "./store";
+import * as actions from './actions'
+
+
+const unsubscribe = store.subscribe(() => {
+    console.log('Store changed', store.getState());
+})
+
+
+store.dispatch(actions.addBug('bug1'))
+store.dispatch(actions.addBug('bug2'))
+store.dispatch(actions.resolveBug(1))
+unsubscribe()
+// store.dispatch(actions.removeBug(1))
+console.log(store.getState());
+ 
